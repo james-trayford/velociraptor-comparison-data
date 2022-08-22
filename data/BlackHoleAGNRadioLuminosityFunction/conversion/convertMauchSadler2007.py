Correct units, remove unused variables, associate redshift intervals

data/BlackHoleAGNRadioLuminosityFunction/conversion/convertMauchSadler2007.py
@@ -11,8 +11,6 @@ with open(sys.argv[1], "r") as handle:
 
 input_filename = "../raw/MauchSadler2007.txt"
 delimiter = None
-half_mass = 1
-log_mass = 0
 
 output_filename = "MauchSadler2007_Data.hdf5"
 output_directory = "../"
@@ -25,13 +23,13 @@ processed = ObservationalData()
 # Read the data (only those columns we need here)
 raw = np.loadtxt(input_filename, delimiter=delimiter, usecols=(0, 1, 2, 3, 4, 5))
 
-L1_4 = 10 ** raw[:, 0] * unyt.dimensionless
-L1_4_low = 10 ** (raw[:, 0] - raw[:, 2]) * unyt.dimensionless
-L1_4_high = 10 ** (raw[:, 0] + raw[:, 1]) * unyt.dimensionless
+L1_4 = 10 ** raw[:, 0] * unyt.Watt / unyt.Hertz
+L1_4_low = 10 ** (raw[:, 0] - raw[:, 2]) * unyt.Watt / unyt.Hertz
+L1_4_high = 10 ** (raw[:, 0] + raw[:, 1]) * unyt.Watt / unyt.Hertz
 
-Phi = 2.51 * 10 ** raw[:, 3] * unyt.Mpc ** -3
-Phi_low = 2.51 * 10 ** (raw[:, 3] - raw[:, 5]) * unyt.Mpc ** -3
-Phi_high = 2.51 * 10 ** (raw[:, 3] + raw[:, 4]) * unyt.Mpc ** -3
+Phi = 2.51 * 10 ** raw[:, 3] / unyt.Mpc ** 3
+Phi_low = 2.51 * 10 ** (raw[:, 3] - raw[:, 5]) / unyt.Mpc ** 3
+Phi_high = 2.51 * 10 ** (raw[:, 3] + raw[:, 4]) / unyt.Mpc ** 3
 
 # Define the scatter as offset from the mean value
 x_scatter = unyt.unyt_array((L1_4 - L1_4_low, L1_4_high - L1_4))
@@ -45,8 +43,9 @@ citation = "Mauch & Sadler (2007)"
 bibcode = "2007MNRAS.375..931M,"
 name = "AGN Radio Luminosity Function"
 plot_as = "points"
-redshift = 0.0
-h = cosmology.h
+redshift = 0.1
+redshift_high = 0.3
+redshift_low = 0.0
 
 processed.associate_x(
     L1_4,
@@ -63,7 +62,7 @@ processed.associate_y(
 processed.associate_citation(citation, bibcode)
 processed.associate_name(name)
 processed.associate_comment(comment)
-processed.associate_redshift(redshift)
+processed.associate_redshift(redshift, redshift_high, redshift_low)
 processed.associate_plot_as(plot_as)
 processed.associate_cosmology(cosmology)
 

data/BlackHoleAGNRadioLuminosityFunction/conversion/convertSmolcic2009.py
@@ -1,5 +1,4 @@
 from velociraptor.observations.objects import ObservationalData
-from astropy.cosmology import WMAP7 as cosmology
 
 import unyt
 import numpy as np
@@ -12,8 +11,6 @@ with open(sys.argv[1], "r") as handle:
 
 input_filename = "../raw/Smolcic2009.txt"
 delimiter = None
-half_mass = 1
-log_mass = 0
 
 output_filename = "Smolcic2009_Data.hdf5"
 output_directory = "../"
@@ -26,14 +23,13 @@ processed = ObservationalData()
 # Read the data (only those columns we need here)
 raw = np.loadtxt(input_filename, delimiter=delimiter, usecols=(0, 1, 2, 3, 4, 5))
 
-print(raw[:, 0] * 10 ** 21)
-L1_4 = raw[:, 0] * (float(1e21) * unyt.dimensionless)
-L1_4_low = (raw[:, 0] - raw[:, 2]) * (float(1e21) * unyt.dimensionless)
-L1_4_high = (raw[:, 0] + raw[:, 1]) * (float(1e21) * unyt.dimensionless)
+L1_4 = raw[:, 0] * (float(1e21) * unyt.Watt / unyt.Hertz)
+L1_4_low = (raw[:, 0] - raw[:, 2]) * (float(1e21) * unyt.Watt / unyt.Hertz)
+L1_4_high = (raw[:, 0] + raw[:, 1]) * (float(1e21) * unyt.Watt / unyt.Hertz)
 
-Phi = raw[:, 3] * (float(1e-4) * unyt.Mpc ** -3)
-Phi_low = (raw[:, 3] - raw[:, 5]) * (float(1e-4) * unyt.Mpc ** -3)
-Phi_high = (raw[:, 3] + raw[:, 4]) * (float(1e-4) * unyt.Mpc ** -3)
+Phi = raw[:, 3] * (float(1e-4) / unyt.Mpc ** 3)
+Phi_low = (raw[:, 3] - raw[:, 5]) * (float(1e-4) / unyt.Mpc ** 3)
+Phi_high = (raw[:, 3] + raw[:, 4]) * (float(1e-4) / unyt.Mpc ** 3)
 
 # Define the scatter as offset from the mean value
 x_scatter = unyt.unyt_array((L1_4 - L1_4_low, L1_4_high - L1_4))
@@ -47,8 +43,9 @@ citation = "Smolcic et al. (2009)"
 bibcode = "2009ApJ...696...24S"
 name = "AGN Radio Luminosity Function"
 plot_as = "points"
-redshift = 0.23
-h = cosmology.h
+redshift = 0.2
+redshift_high = 0.1
+redshift_low = 0.35
 
 processed.associate_x(
     L1_4,
@@ -65,7 +62,7 @@ processed.associate_y(
 processed.associate_citation(citation, bibcode)
 processed.associate_name(name)
 processed.associate_comment(comment)
-processed.associate_redshift(redshift)
+processed.associate_redshift(redshift, redshift_high, redshift_low)
 processed.associate_plot_as(plot_as)
 processed.associate_cosmology(cosmology)
 

data/BlackHoleMassFunction/conversion/convertMerloniHeinz2008.py
@@ -11,8 +11,6 @@ with open(sys.argv[1], "r") as handle:
 
 input_filename = "../raw/MerloniHeinz2008.txt"
 delimiter = None
-half_mass = 1
-log_mass = 0
 
 output_filename = "MerloniHeinz2008_Data.hdf5"
 output_directory = "../"
@@ -29,15 +27,15 @@ M_BH = 10 ** raw[:, 0] * unyt.Solar_Mass
 M_BH_low = 10 ** (raw[:, 0] - raw[:, 2]) * unyt.Solar_Mass
 M_BH_high = 10 ** (raw[:, 0] + raw[:, 1]) * unyt.Solar_Mass
 
-Phi = 10 ** raw[:, 3] * unyt.Mpc ** -3
-Phi_low = 10 ** (raw[:, 5]) * unyt.Mpc ** -3
-Phi_high = 10 ** (raw[:, 4]) * unyt.Mpc ** -3
+Phi = 10 ** raw[:, 3] / unyt.Mpc ** 3
+Phi_low = 10 ** (raw[:, 5]) / unyt.Mpc ** 3
+Phi_high = 10 ** (raw[:, 4]) / unyt.Mpc ** 3
 
 # Define the scatter as offset from the mean value
 x_scatter = unyt.unyt_array((M_BH - M_BH_low, M_BH_high - M_BH))
 y_scatter = unyt.unyt_array(
     (
-        -(10 ** (2 * np.log10(Phi) - np.log10(Phi_high))) * unyt.Mpc ** -3 + Phi,
+        -(10 ** (2 * np.log10(Phi) - np.log10(Phi_high))) / unyt.Mpc ** 3 + Phi,
         Phi_high - Phi,
     )
 )
@@ -55,7 +53,6 @@ bibcode = "2008MNRAS.388.1011M."
 name = "Black Hole Mass Function"
 plot_as = "points"
 redshift = 0.0
-h = cosmology.h
 
 processed.associate_x(
     M_BH, scatter=x_scatter, comoving=False, description="Black hole mass"