76a6abdf437a1929e05e9e5f6707e008758ada0d
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2022-09-09 16:36:32 +0200

    Extract the sSFR - BH mass relation and not the SFR - BH mass one

--- data/BlackHoleMassSpecificStarFormationRate/conversion/convertTerrazas2017.py
+++ data/BlackHoleMassSpecificStarFormationRate/conversion/convertTerrazas2017.py
@@ -23,8 +23,11 @@ if not os.path.exists(output_directory):
 # Read the data
 data = np.loadtxt(input_filename)
 
-# Stellar masses
+# Stellar masses (uncertainty taken from Sec. 2)
 Mstar = unyt.unyt_array(10 ** data[:, 0], units="Solar_Mass")
+Mstar_hi = unyt.unyt_array(10 ** (data[:, 0] + 0.15), units="Solar_Mass")
+Mstar_lo = unyt.unyt_array(10 ** (data[:, 0] - 0.15), units="Solar_Mass")
+Mstar_scatter = unyt.unyt_array([Mstar - Mstar_lo, Mstar_hi - Mstar])
 
 # Black Hole Masses
 BH_mass = unyt.unyt_array(10 ** data[:, 3], units="Solar_Mass")
@@ -38,6 +41,13 @@ SFR_hi = unyt.unyt_array(10 ** (data[:, 1] + 0.3), units="Solar_Mass / year")
 SFR_lo = unyt.unyt_array(10 ** (data[:, 1] - 0.3), units="Solar_Mass / year")
 SFR_scatter = unyt.unyt_array([SFR - SFR_lo, SFR_hi - SFR])
 
+# Specific Star formation rates
+sSFR = SFR / Mstar
+sSFR_hi = unyt.unyt_array(10 ** (np.log10(sSFR) + 0.45), units="year**-1")
+sSFR_lo = unyt.unyt_array(10 ** (np.log10(sSFR) - 0.45), units="year**-1")
+sSFR_scatter = unyt.unyt_array([sSFR - sSFR_lo, sSFR_hi - sSFR])
+
+
 # Upper limits
 limits = data[:, 2]
 
@@ -48,7 +58,7 @@ comment = (
 )
 citation = f"Terrazas (2017) "
 bibcode = "2017ApJ...844..170T"
-name = "Black hole mass - Star Formation Rate relation"
+name = "Black hole mass - Specific Star Formation Rate relation"
 plot_as = "points"
 # We purposely make this data show up not only a z=0 but also at higher z
 redshift_lower, redshift_upper = -0.1, 3.1
@@ -64,7 +74,10 @@ processed.associate_x(
     description="Black Hole Mass",
 )
 processed.associate_y(
-    SFR, scatter=SFR_scatter, comoving=True, description="Star Formation Rate"
+    sSFR,
+    scatter=sSFR_scatter,
+    comoving=True,
+    description="Specific Star Formation Rate",
 )
 processed.associate_citation(citation, bibcode)
 processed.associate_name(name)
