49a25a3d6bf09a14594d6f3bf78240efc21d3f3a
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2022-10-17 18:02:58 +0200

    Change the BlackHoleMassSpecificStarFormationRate script to write one file with the data without upper limits and one with upper limits

--- data/BlackHoleMassSpecificStarFormationRate/conversion/convertTerrazas2017.py
+++ data/BlackHoleMassSpecificStarFormationRate/conversion/convertTerrazas2017.py
@@ -14,12 +14,6 @@ h_sim = cosmology.h
 
 input_filename = "../raw/Terrazas17.dat"
 
-output_filename = "Terrazas2017.hdf5"
-output_directory = "../"
-
-if not os.path.exists(output_directory):
-    os.mkdir(output_directory)
-
 # Read the data
 data = np.loadtxt(input_filename)
 
@@ -42,11 +36,26 @@ BH_mass_lo = unyt.unyt_array(10 ** (data[:, 3] - data[:, 4]), units="Solar_Mass"
 BH_mass_scatter = unyt.unyt_array([BH_mass - BH_mass_lo, BH_mass_hi - BH_mass])
 
 # Upper limits
-limits = data[:, 2]
+upper_limits = np.array(data[:, 2], dtype=bool)
+print(upper_limits)
+no_limits = np.logical_not(upper_limits)
+
+print(no_limits)
+
+# First, data without upper limits #########################
+BH_mass_no_limit = BH_mass[no_limits]
+BH_mass_scatter_no_limit = unyt.unyt_array(
+    [BH_mass_scatter[0][no_limits], BH_mass_scatter[1][no_limits]]
+)
+sSFR_no_limit = sSFR[no_limits]
+sSFR_scatter_no_limit = unyt.unyt_array(
+    [sSFR_scatter[0][no_limits], sSFR_scatter[1][no_limits]]
+)
 
 # Meta-data
 comment = (
     "Selection of local galaxies with masses > 10^10 Msun. "
+    "Data with upper limits removed. "
     f"No cosmology correction needed."
 )
 citation = f"Terrazas (2017) "
@@ -61,14 +70,14 @@ h = h_sim
 # Write everything
 processed = ObservationalData()
 processed.associate_x(
-    BH_mass,
-    scatter=BH_mass_scatter,
+    BH_mass_no_limit,
+    scatter=BH_mass_scatter_no_limit,
     comoving=True,
     description="Black Hole Mass",
 )
 processed.associate_y(
-    sSFR,
-    scatter=sSFR_scatter,
+    sSFR_no_limit,
+    scatter=sSFR_scatter_no_limit,
     comoving=True,
     description="Specific Star Formation Rate",
 )
@@ -78,6 +87,71 @@ processed.associate_comment(comment)
 processed.associate_redshift(redshift, redshift_lower, redshift_upper)
 processed.associate_plot_as(plot_as)
 processed.associate_cosmology(cosmology)
+
+output_filename = "Terrazas2017.hdf5"
+output_directory = "../"
+
+if not os.path.exists(output_directory):
+    os.mkdir(output_directory)
+
+output_path = f"{output_directory}/{output_filename}"
+
+if os.path.exists(output_path):
+    os.remove(output_path)
+
+processed.write(filename=output_path)
+
+# Second, data with upper limits #########################
+BH_mass_upper_limit = BH_mass[upper_limits]
+BH_mass_scatter_upper_limit = unyt.unyt_array(
+    [BH_mass_scatter[0][upper_limits], BH_mass_scatter[1][upper_limits]]
+)
+sSFR_upper_limit = sSFR[upper_limits]
+sSFR_scatter_upper_limit = unyt.unyt_array(
+    [sSFR_scatter[0][upper_limits], sSFR_scatter[1][upper_limits]]
+)
+
+# Meta-data
+comment = (
+    "Selection of local galaxies with masses > 10^10 Msun. "
+    "Only upper limits considered. "
+    f"No cosmology correction needed."
+)
+citation = f"Terrazas (2017) - upper limits"
+bibcode = "2017ApJ...844..170T"
+name = "Black hole mass - Specific Star Formation Rate relation"
+plot_as = "points"
+# We purposely make this data show up not only a z=0 but also at higher z
+redshift_lower, redshift_upper = -0.1, 3.1
+redshift = 0.0
+h = h_sim
+
+# Write everything
+processed = ObservationalData()
+processed.associate_x(
+    BH_mass_upper_limit,
+    scatter=BH_mass_scatter_upper_limit,
+    comoving=True,
+    description="Black Hole Mass",
+)
+processed.associate_y(
+    sSFR_upper_limit,
+    scatter=None,
+    comoving=True,
+    description="Specific Star Formation Rate",
+)
+processed.associate_citation(citation, bibcode)
+processed.associate_name(name)
+processed.associate_comment(comment)
+processed.associate_redshift(redshift, redshift_lower, redshift_upper)
+processed.associate_plot_as(plot_as)
+processed.associate_cosmology(cosmology)
+
+output_filename = "Terrazas2017_upper_limits.hdf5"
+output_directory = "../"
+
+if not os.path.exists(output_directory):
+    os.mkdir(output_directory)
 
 output_path = f"{output_directory}/{output_filename}"
 
