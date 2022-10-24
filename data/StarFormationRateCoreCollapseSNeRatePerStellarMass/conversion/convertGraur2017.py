71a739d858c84a4dd1026664784ad071fdc30f2c
fonotec <fonotec@hotmail.com>
2022-10-24 11:10:31 +0200

    Update graur+2017 file

--- data/StarFormationRateCoreCollapseSNeRatePerStellarMass/conversion/convertGraur2017.py
+++ data/StarFormationRateCoreCollapseSNeRatePerStellarMass/conversion/convertGraur2017.py
@@ -9,113 +9,80 @@ import sys
 with open(sys.argv[1], "r") as handle:
     exec(handle.read())
 
-input_filename = "../raw/SE_SNe_rate_vs_SFR_Graur_2017.txt"
+input_filename_array = [
+    "../raw/SE_SNe_rate_vs_SFR_Graur_2017.txt",
+    "../raw/SNII_rate_vs_SFR_Graur_2017.txt",
+]
+output_filename_array = [
+    "Graur2017_SE_SNe.hdf5",
+    "Graur2017_SNII.hdf5",
+]
 
-output_filename = "Graur2017_SE_SNe.hdf5"
-output_directory = "../"
+for i in range(0, len(input_filename_array)):
 
-if not os.path.exists(output_directory):
-    os.mkdir(output_directory)
+    input_filename = input_filename_array[i]
 
-processed = ObservationalData()
-raw = np.loadtxt(input_filename)
+    output_filename = output_filename_array[i]
+    output_directory = "../"
 
-comment = "LOSS [$z \\approx 0.2$]"
-citation = "Graur et al. (2017)"
-bibcode = "2017ApJ...837..120G"
-name = "Star Formation Rates-SE SNe Rate per Stellar Mass"
-plot_as = "points"
-redshift = 0.2
-h_obs = 0.7
-h = cosmology.h
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
 
-SFR = unyt.unyt_array(10 ** raw.T[0], units="Msun/year")
-SNuM = unyt.unyt_array(raw.T[3] * 1e-12, units="yr**(-1) * Msun**(-1)")
+    processed = ObservationalData()
+    raw = np.loadtxt(input_filename)
 
-SNuM_err = unyt.unyt_array(
-    [
-        raw.T[4] * 1e-12,
-        raw.T[5] * 1e-12,
-    ],
-    units="yr**(-1) * Msun**(-1)",
-)
+    comment = "LOSS [$z \\approx 0.2$]"
+    citation = "Graur et al. (2017)"
+    bibcode = "2017ApJ...837..120G"
+    if i==0:
+        name = "Star Formation Rates-SE SN Rate per Stellar Mass"
+    elif i==1:
+        name = "Star Formation Rates-SNII Rate per Stellar Mass"
+    plot_as = "points"
+    redshift = 0.2
+    h_obs = 0.7
+    h = cosmology.h
 
-SFR_err = unyt.unyt_array(
-    [
-        10 ** (raw.T[0]) - 10 ** (raw.T[0] - raw.T[1]),
-        10 ** (raw.T[0] + raw.T[2]) - 10 ** (raw.T[0]),
-    ],
-    units="Msun/year",
-)
+    SFR = unyt.unyt_array(10 ** raw.T[0], units="Msun/year")
+    SNuM = unyt.unyt_array(raw.T[3] * 1e-12, units="yr**(-1) * Msun**(-1)")
 
-processed.associate_x(
-    SFR, scatter=SFR_err, comoving=True, description="Star Formation rate"
-)
-processed.associate_y(
-    SNuM, scatter=SNuM_err, comoving=False, description="SE SNe rate per stellar mass"
-)
-processed.associate_citation(citation, bibcode)
-processed.associate_name(name)
-processed.associate_comment(comment)
-processed.associate_redshift(redshift)
-processed.associate_plot_as(plot_as)
-processed.associate_cosmology(cosmology)
+    SNuM_err = unyt.unyt_array(
+        [
+            raw.T[4] * 1e-12,
+            raw.T[5] * 1e-12,
+        ],
+        units="yr**(-1) * Msun**(-1)",
+    )
 
-output_path = f"{output_directory}/{output_filename}"
+    SFR_err = unyt.unyt_array(
+        [
+            10 ** (raw.T[0]) - 10 ** (raw.T[0] - raw.T[1]),
+            10 ** (raw.T[0] + raw.T[2]) - 10 ** (raw.T[0]),
+        ],
+        units="Msun/year",
+    )
 
-if os.path.exists(output_path):
-    os.remove(output_path)
+    processed.associate_x(
+        SFR, scatter=SFR_err, comoving=True, description="Star Formation rate"
+    )
+    if i==0:
+        processed.associate_y(
+            SNuM, scatter=SNuM_err, comoving=False, description="SE SNe rate per stellar mass"
+        )
+    elif i==1:
+        processed.associate_y(
+            SNuM, scatter=SNuM_err, comoving=False, description="SNII rate per stellar mass"
+        )
+    processed.associate_citation(citation, bibcode)
+    processed.associate_name(name)
+    processed.associate_comment(comment)
+    processed.associate_redshift(redshift)
+    processed.associate_plot_as(plot_as)
+    processed.associate_cosmology(cosmology)
 
-processed.write(filename=output_path)
+    output_path = f"{output_directory}/{output_filename}"
 
-input_filename = "../raw/SNII_rate_vs_SFR_Graur_2017.txt"
+    if os.path.exists(output_path):
+        os.remove(output_path)
 
-output_filename = "Graur2017_SNII.hdf5"
-output_directory = "../"
-
-if not os.path.exists(output_directory):
-    os.mkdir(output_directory)
-
-processed = ObservationalData()
-raw = np.loadtxt(input_filename)
-
-name = "Star Formation Rates-SNII Rate per Stellar Mass"
-
-SFR = unyt.unyt_array(10 ** raw.T[0], units="Msun/year")
-SNuM = unyt.unyt_array(raw.T[3] * 1e-12, units="yr**(-1) * Msun**(-1)")
-
-SNuM_err = unyt.unyt_array(
-    [
-        raw.T[4] * 1e-12,
-        raw.T[5] * 1e-12,
-    ],
-    units="yr**(-1) * Msun**(-1)",
-)
-
-SFR_err = unyt.unyt_array(
-    [
-        10 ** (raw.T[0]) - 10 ** (raw.T[0] - raw.T[1]),
-        10 ** (raw.T[0] + raw.T[2]) - 10 ** (raw.T[0]),
-    ],
-    units="Msun/year",
-)
-
-processed.associate_x(
-    SFR, scatter=SFR_err, comoving=True, description="Star Formation rate"
-)
-processed.associate_y(
-    SNuM, scatter=SNuM_err, comoving=False, description="SNII rate per stellar mass"
-)
-processed.associate_citation(citation, bibcode)
-processed.associate_name(name)
-processed.associate_comment(comment)
-processed.associate_redshift(redshift)
-processed.associate_plot_as(plot_as)
-processed.associate_cosmology(cosmology)
-
-output_path = f"{output_directory}/{output_filename}"
-
-if os.path.exists(output_path):
-    os.remove(output_path)
-
-processed.write(filename=output_path)
+    processed.write(filename=output_path)
