f4a060db1c3be2687c2412de4eac87ab9a3551ec
fonotec <fonotec@hotmail.com>
2022-10-26 10:19:32 +0200

    Update gas metallicity vs CC SNe rates Graur+2017

--- data/GalaxyGasMetallicityCoreCollapseSNeRatePerStellarMass/conversion/convertGraur2017.py
+++ data/GalaxyGasMetallicityCoreCollapseSNeRatePerStellarMass/conversion/convertGraur2017.py
@@ -10,20 +10,17 @@ import sys
 with open(sys.argv[1], "r") as handle:
     exec(handle.read())
 
-input_filename_array = [
-    "../raw/SE_SNe_rate_vs_gas_metallicity_Graur_2017.txt",
-    "../raw/SNII_rate_vs_gas_metallicity_Graur_2017.txt",
-]
-output_filename_array = [
-    "Graur2017_SE_SNe.hdf5",
-    "Graur2017_SNII.hdf5",
+
+variations = [
+    ("SE_SNe", "SE SN Rate per Stellar Mass"),
+    ("SNII", "SNII Rate per Stellar Mass"),
 ]
 
-for i in range(0, len(input_filename_array)):
+for file_prefix, description in variations:
 
-    input_filename = input_filename_array[i]
+    input_filename = f"../raw/{file_prefix}_rate_vs_gas_metallicity_Graur_2017.txt"
 
-    output_filename = output_filename_array[i]
+    output_filename = f"Graur2017_{file_prefix}.hdf5"
     output_directory = "../"
 
     if not os.path.exists(output_directory):
@@ -35,10 +32,7 @@ for i in range(0, len(input_filename_array)):
     comment = "LOSS [$z \\approx 0.2$]"
     citation = "Graur et al. (2017)"
     bibcode = "2017ApJ...837..120G"
-    if i == 0:
-        name = "Gas metallicity - SE SN Rate per Stellar Mass"
-    elif i == 1:
-        name = "Gas metallicity - SNII Rate per Stellar Mass"
+    name = f"Gas metallicity - {description}"
     plot_as = "points"
     redshift = 0.2
     h_obs = 0.7
@@ -66,20 +60,13 @@ for i in range(0, len(input_filename_array)):
     processed.associate_x(
         gasZ, scatter=gasZ_err, comoving=False, description="SF Gas 12+log(O/H) from Z"
     )
-    if i == 0:
-        processed.associate_y(
-            SNuM,
-            scatter=SNuM_err,
-            comoving=False,
-            description="SE SNe rate per stellar mass",
-        )
-    elif i == 1:
-        processed.associate_y(
-            SNuM,
-            scatter=SNuM_err,
-            comoving=False,
-            description="SNII rate per stellar mass",
-        )
+    processed.associate_y(
+        SNuM,
+        scatter=SNuM_err,
+        comoving=False,
+        description=f"{description}",
+    )
+
     processed.associate_citation(citation, bibcode)
     processed.associate_name(name)
     processed.associate_comment(comment)
