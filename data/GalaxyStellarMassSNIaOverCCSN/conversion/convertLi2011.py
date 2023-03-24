cabd2c0f424315dba748212a6fab53f7612834e3
fonotec <fonotec@hotmail.com>
2023-03-24 11:34:52 +0100

    Code formatting

--- data/GalaxyStellarMassSNIaOverCCSN/conversion/convertLi2011.py
+++ data/GalaxyStellarMassSNIaOverCCSN/conversion/convertLi2011.py
@@ -42,9 +42,9 @@ SNIa_err = unyt.unyt_array(
     units="yr * yr**(-1)",
 )
 
-SNIa_rate = SNIa_rate / (1. + SNIa_rate)
+SNIa_rate = SNIa_rate / (1.0 + SNIa_rate)
 
-SNIa_err = SNIa_err / (1. + SNIa_rate)
+SNIa_err = SNIa_err / (1.0 + SNIa_rate)
 
 dMstar = 0.25 / 2.0
 Mstar_err = unyt.unyt_array(
